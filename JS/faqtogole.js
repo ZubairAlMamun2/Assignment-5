97b0f32d8ece569a0c4cdac13832610808a9e547
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-09-23 13:55:47 +0600

    everithing up to date

---
+++ JS/faqtogole.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById("blog-page").addEventListener("click",function(){
+    window.location.href="faq.html"
+})
+document.getElementById("blog-page-small").addEventListener("click",function(){
+    window.location.href="faq.html"
+})
+
+
+document.getElementById("home-page").addEventListener("click",function(){
+    window.location.href="index.html"
+})
+document.getElementById("home-page-small").addEventListener("click",function(){
+    window.location.href="index.html"
+})
--- JS/togole.js
+++ JS/togole.js
@@ -2,7 +2,7 @@ const donateButton =document.getElementById("donate-button");
 const historyButton =document.getElementById("History-button");
 
 historyButton.addEventListener("click",function(){
-    console.log("clicked")
+    
     historyButton.classList="py-3 px-8 bg-bg-green rounded-lg text-text-primary border";
     donateButton.classList="py-3 px-8 bg-primary rounded-lg text-text-secondary border";
     document.getElementById("firstdiv").classList="hidden";
@@ -12,7 +12,7 @@ historyButton.addEventListener("click",function(){
     
 })
 donateButton.addEventListener("click",function(){
-    console.log("clicked")
+    
     historyButton.classList="py-3 px-8 bg-primary rounded-lg text-text-secondary border";
     donateButton.classList="py-3 px-8 bg-bg-green rounded-lg text-text-primary border";
     document.getElementById("firstdiv").classList="grid grid-cols-1 md:grid-cols-2 border rounded-2xl p-8 mt-8 mx-4 gap-8";
@@ -20,3 +20,7 @@ donateButton.addEventListener("click",function(){
     document.getElementById("thirddiv").classList="grid grid-cols-1 md:grid-cols-2 border rounded-2xl p-8 mt-8 mx-4 gap-8";
     document.getElementById("History").classList="hidden";
 })
+
+
+
+
